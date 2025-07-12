1ed17ac3d709dcbc2c99b38ccad98a57c6c0fa7c
SpencerSedano <spencer.sv20@gmail.com>
2025-07-12 11:04:46 +0800

    contacts2 update

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -2,6 +2,7 @@ const mongodb = require("../data/database");
 const ObjectId = require("mongodb").ObjectId;
 
 const getAll = async (req, res) => {
+  //#swagger.tags = ['Contacts']
   const result = await mongodb.getDatabase().db().collection("contacts").find();
   result.toArray().then((contacts) => {
     res.setHeader("Content-Type", "application/json");
@@ -10,6 +11,7 @@ const getAll = async (req, res) => {
 };
 
 const getSingle = async (req, res) => {
+  //#swagger.tags = ['Contacts']
   const userId = new ObjectId(req.params.id);
   const result = await mongodb
     .getDatabase()
@@ -22,7 +24,98 @@ const getSingle = async (req, res) => {
   });
 };
 
+const createContact = async (req, res) => {
+  //#swagger.tags = ['Contacts']
+  try {
+    const user = {
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email,
+      phone: req.body.phone,
+      address: {
+        street: req.body.address.street,
+        city: req.body.address.city,
+        state: req.body.address.state,
+        zip: req.body.address.zip,
+      },
+      company: req.body.company,
+      createdAt: new Date(),
+    };
+
+    const response = await mongodb
+      .getDatabase()
+      .db()
+      .collection("contacts")
+      .insertOne(user);
+
+    if (response.acknowledged) {
+      res
+        .status(201)
+        .json({ message: "Contact created", id: response.insertedId });
+    } else {
+      res.status(500).json({ error: "Failed to create contact" });
+    }
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+const updateContact = async (req, res) => {
+  //#swagger.tags = ['Contacts']
+  const userId = new ObjectId(req.params.id);
+  const user = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    phone: req.body.phone,
+    address: {
+      street: req.body.address.street,
+      city: req.body.address.city,
+      state: req.body.address.state,
+      zip: req.body.address.zip,
+    },
+    company: req.body.company,
+    createdAt: req.body.createdAt,
+  };
+  const response = await mongodb
+    .getDatabase()
+    .db()
+    .collection("contacts")
+    .replaceOne({ _id: userId }, user);
+
+  if (response.modifiedCount > 0) {
+    res.status(204).send();
+  } else {
+    res
+      .status(500)
+      .json(response.error || "Some error occuring while updating user");
+  }
+};
+
+const deleteContact = async (req, res) => {
+  //#swagger.tags = ['Contacts']
+  try {
+    const userId = new ObjectId(req.params.id);
+    const response = await mongodb
+      .getDatabase()
+      .db()
+      .collection("contacts")
+      .deleteOne({ _id: userId });
+
+    if (response.deletedCount > 0) {
+      res.status(204).send();
+    } else {
+      res.status(404).json({ error: "Contact not found" });
+    }
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
 module.exports = {
   getAll,
   getSingle,
+  createContact,
+  updateContact,
+  deleteContact,
 };
